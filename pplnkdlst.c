0a5d1648d1c3c6c6a12c82e5352c4655028142a9
Kudipudi Sai Kiran <160224020+kudipudisaikiran@users.noreply.github.com>
2024-04-12 16:47:24 +0530

    Add files via upload
    By using linkedlist

---
+++ pplnkdlst.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+int data;
+struct node*next;
+}*top==NULL;
+
+void push(int d);
+void push(int d)
+{
+struct node*temp=malloc(size of (struct  node*))
+temp->data=d;
+temp->next=NULL;
+temp->next=top;
+top=temp;
+}
+int display();
+int display()
+{
+printf("%d\n",top->data);
+top+topp->next;
+}
+}
+int pop();
+int main
+{
+push(60);
+dsplay;
+}
